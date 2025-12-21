04727ce3986c0e3bc19778665db941d804ef842c
Murad Khalitov <darggun@gmail.com>
2025-12-21 17:21:20 +0300

    feat: Add unit and integration tests for service and DAO layers       Write tests for UserService and UserDao       Implement JUnit, Mockito, and Testcontainers tests

--- src/main/java/ru/murad/dao/AbstractHibernateDao.java
+++ src/main/java/ru/murad/dao/AbstractHibernateDao.java
@@ -1,100 +1,99 @@
 package ru.murad.dao;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import ru.murad.exception.DaoException;
-import ru.murad.util.HibernateUtil;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public abstract class AbstractHibernateDao<T, ID> implements Dao<T, ID> {
 
-    private final Class<T> clazz;
+    protected final SessionFactory sessionFactory;
+    protected final Class<T> clazz;
 
-    protected AbstractHibernateDao(Class<T> clazz) {
+    protected AbstractHibernateDao(Class<T> clazz, SessionFactory sessionFactory) {
         this.clazz = clazz;
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public T save(T entity) {
-        Transaction tx = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
-            tx = session.beginTransaction();
+        return executeInTransaction(session -> {
             session.persist(entity);
-            tx.commit();
-
             return entity;
-
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            throw new DaoException("Error saving " + clazz.getSimpleName(), e);
-        }
+        }, "saving");
     }
 
     @Override
     public T update(T entity) {
-        Transaction tx = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
-            tx = session.beginTransaction();
-            T merged = session.merge(entity);
-            tx.commit();
-
-            return merged;
-
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            throw new DaoException("Error updating " + clazz.getSimpleName(), e);
-        }
+        return executeInTransaction(session -> session.merge(entity), "updating");
     }
 
     @Override
     public boolean delete(ID id) {
-        Transaction tx = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
-            tx = session.beginTransaction();
-
+        return executeInTransaction(session -> {
             T entity = session.find(clazz, id);
             if (entity == null) {
                 return false;
             }
-
             session.remove(entity);
-            tx.commit();
-
             return true;
-
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            throw new DaoException("Error deleting " + clazz.getSimpleName(), e);
-        }
+        }, "deleting id=" + id);
     }
 
     @Override
     public Optional<T> findById(ID id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
-            T entity = session.find(clazz, id);
-            return Optional.ofNullable(entity);
-
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.find(clazz, id));
         } catch (Exception e) {
-            throw new DaoException("Error finding " + clazz.getSimpleName(), e);
+            throw new DaoException(
+                    "Error finding " + entityName() + " with id=" + id, e
+            );
         }
     }
 
     @Override
     public List<T> findAll() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-
+        try (Session session = sessionFactory.openSession()) {
+            String entityName = entityName();
             return session.createQuery(
-                    "SELECT e FROM " + clazz.getSimpleName() + " e", clazz
+                    "select e from " + entityName + " e", clazz
             ).getResultList();
-
         } catch (Exception e) {
-            throw new DaoException("Error loading all " + clazz.getSimpleName(), e);
+            throw new DaoException(
+                    "Error loading all " + entityName(), e
+            );
         }
+    }
+
+    protected <R> R executeInTransaction(
+            Function<Session, R> action,
+            String operation
+    ) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            R result = action.apply(session);
+            tx.commit();
+            return result;
+        } catch (Exception e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw new DaoException(
+                    "Error " + operation + " " + entityName(), e
+            );
+        }
+    }
+
+    protected String entityName() {
+        return sessionFactory
+                .getMetamodel()
+                .entity(clazz)
+                .getName();
     }
 }
